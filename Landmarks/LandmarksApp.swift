18ed3aeb48b7855b6af6a8026509c0e1b8159d1a
mikio <43379090+mikiolight@users.noreply.github.com>
2025-09-05 00:05:49 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mikihisa Saito on 2025/09/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
