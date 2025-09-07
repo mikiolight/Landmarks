c77a41d6276008700b15188bb95aef7c94c66cef
mikio <43379090+mikiolight@users.noreply.github.com>
2025-09-07 15:14:08 +0900

    Complete Building lists and navigation

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
+    
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())            .overlay{
                 Circle().stroke(.white, lineWidth:4)
             }
@@ -18,5 +20,5 @@ struct CircleImage: View {
 }
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
